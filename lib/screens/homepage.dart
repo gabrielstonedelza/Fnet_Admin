af9e54089c187b3c6eb83673f31ceb347f1cc2aa
Gabriel Akwasi Asare <gabrielstonedelza@gmail.com>
2023-08-29 10:19:24 +0000

    fnet flutter admin app, another change to homepage background calling

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -193,9 +193,9 @@ class _HomePageState extends State<HomePage> {
       var jsonData = const Utf8Decoder().convert(codeUnits);
       var deData = json.decode(jsonData);
       unPaidDepositRequests.assignAll(deData);
-      setState(() {
-        isLoading = false;
-      });
+      // setState(() {
+      //   isLoading = false;
+      // });
     } else {
       if (kDebugMode) {
         print(response.body);
